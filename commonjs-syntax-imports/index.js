d1c120e496cfd3a748bb8b3bb79778dd23ad6e8d
Sashankr <sashankr97@gmail.com>
2022-05-09 12:47:07 +0530

    common js import syntax

--- commonjs-syntax-imports/index.js
+++ commonjs-syntax-imports/index.js
@@ -2,3 +2,5 @@ const util = require('./util')
 
 const newSum = util.addNumbers(10,12.5);
 console.log(newSum);
+
+console.log(util.subtractNumbers(1,2));
--- commonjs-syntax-imports/util.js
+++ commonjs-syntax-imports/util.js
@@ -2,4 +2,9 @@ const addNumbers = (a,b) =>{
     return a + b
 }
 
+const subtractNumbers = (a,b) =>{
+    return a > b ? a - b : b - a;
+}
+
 module.exports.addNumbers = addNumbers;
+module.exports.subtractNumbers = subtractNumbers;
