bc078f352bc0caa4a34f51b68c22f875c73ac0dd
Sashankr <sashankr97@gmail.com>
2022-05-10 00:17:05 +0530

    sending file from server

--- Servers/serverWithExpress.js
+++ Servers/serverWithExpress.js
@@ -1,40 +1,42 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const PORT = 8080;
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('hello world');
 })
 
-app.get('/what-is-my-method',(req,res)=>{
+app.get('/what-is-my-method', (req, res) => {
     res.send('GET');
 })
 
-app.post('/what-is-my-method',(req,res)=>{
+app.post('/what-is-my-method', (req, res) => {
     res.send('POST');
 })
 
-app.put('/what-is-my-method',(req,res)=>{
+app.put('/what-is-my-method', (req, res) => {
     res.send('PUT');
 })
 
 
-app.delete('/what-is-my-method',(req,res)=>{
+app.delete('/what-is-my-method', (req, res) => {
     res.send('DELETE');
 })
 
-app.patch('/what-is-my-method',(req,res)=>{
+app.patch('/what-is-my-method', (req, res) => {
     res.send('PATCH');
 })
 
-app.post('/something',(req,res)=>{
+app.post('/something', (req, res) => {
     res.send('This is usually not visible')
 })
 
-fetch('/something',{
-    method : 'POST'
-}).then(res =>{
-    console.log(res.text()); // to access the text sent in post we need to fetch it and print the text
-})
+app.listen(PORT);
 
-app.listen(process.env.PUBLIC_PORT);
+console.log(PORT);
+
+app.get('/example', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
