4787b4fb953a58d906d6fcf8ba377a0c3eefc829
Sashankr <sashankr97@gmail.com>
2022-05-09 14:31:00 +0530

    learned about default and named exports

---
+++ es6-modules/index.mjs
@@ -0,0 +1,6 @@
+//default import
+// import square from "./module.mjs";
+
+import {square,cube} from './module.mjs'
+
+console.log(square(5),cube(5));
---
+++ es6-modules/module.mjs
@@ -0,0 +1,16 @@
+function square(num){
+    return num ** 2;
+}
+
+function cube(num){
+    return num ** 3;
+}
+
+
+// Default export
+// export default square;
+
+export {
+    square,
+    cube
+}
